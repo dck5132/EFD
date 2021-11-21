5522114dfd62dfcec4fc6a35a04e89a76960c9c4
Cyril <dck5132@gmail.com>
2021-11-21 00:43:09 -0500

    Update legend for mobile and update version number to 1.1.5

--- src/app/components/high-pie-chart/high-pie-chart.component.ts
+++ src/app/components/high-pie-chart/high-pie-chart.component.ts
@@ -100,7 +100,7 @@ export class HighPieChartComponent implements OnInit, OnChanges, OnDestroy {
         margin: 25,
         title: {
           text:
-            '<div class="d-flex justify-content-center align-items-center"><div class="legend text-center legend-width fw-bold">Please select maps you wish to ignore:</div></div>'
+            '<div class="d-flex align-items-center"><div class="legend fw-bold">Please select maps you wish to ignore:</div></div>'
         },
         itemStyle: {
           color: 'white',
