dc78778d891d67d50d104e9a6431b56d2e333e5c
Cyril Kalikin <dck5132@gmail.com>
2024-08-25 21:00:47 -0400

    Update chart.constants.ts
    Added new maps to constants

--- src/app/constants/chart.constants.ts
+++ src/app/constants/chart.constants.ts
@@ -3,37 +3,52 @@ import { MapData } from "../interfaces/map-data";
 export const Maps: MapData[] = [
     {
       name: 'Interchange',
-      y: 14.285,
+      y: 10,
       color: '#AF00FF'
     },
     {
       name: 'Woods',
-      y: 14.285,
+      y: 10,
       color: '#0025FF'
     },
     {
       name: 'Customs',
-      y: 14.285,
+      y: 10,
       color: '#FF0000'
     },
     {
       name: 'Shoreline',
-      y: 14.285,
+      y: 10,
       color: '#039000'
     },
     {
       name: 'The Lab',
-      y: 14.285,
+      y: 10,
       color: '#2BC5F1'
     },
     {
       name: 'Factory',
-      y: 14.285,
+      y: 10,
       color: '#FBB9FA'
     },
     {
+      name: 'Lighthouse',
+      y: 10,
+      color: '#777c44'
+    },
+    {
+      name: 'Streets',
+      y: 10,
+      color: '#98acaa'
+    },
+    {
+      name: 'Ground Zero',
+      y: 10,
+      color: '#4d3f2c'
+    },
+    {
       name: 'Reserve',
-      y: 14.285,
+      y: 10,
       color: '#FF9100'
     }
   ];
